f0e7f96af6d20402f9bd6192f5789197b87aee49
ghostff <frankchris@hotmail.com>
2016-06-22 23:44:57 -0500

    psr standard

--- function.php
+++ function.php
@@ -7,54 +7,55 @@ class Dump
     const DATA_N    = '#bbbbbb';//default gray (for 'string')
     const DATA_TY   = '#5ba415';//default lemon (for lenght or size)
     const N_ARRAY   = '#000000';//default black (for array or objects)
-    const BOOL	    = '#bb02ff';//default light purple (for bool)
+    const BOOL      = '#bb02ff';//default light purple (for bool)
     const D_NULL    = '#6789f8';//default light blue (for null)
     const FLOT      = '#9c6e25';//default brown (for float)
     const PNT       = '#f00000';//default red (for refrences like '=>' and ':')
     const NPNT      = '#e103c4';//default pink (for '=')
     const INTE      = '#1baabb';//default greenishblue (for int)
     const A_PT      = '#59829e';//default light navy blue (for array key)
-	const VISIB     = '#741515';//default dark red (for object visibility)
-	const VAR_N     = '#987a00';//default light brown (for object variable name)
+    const VISIB     = '#741515';//default dark red (for object visibility)
+    const VAR_N     = '#987a00';//default light brown (for object variable name)
     
     private $marg = 20;
     private $arr_count = null;
     private $detem_last = 1;
     private $proc_end = false;
-	private $instance = true;
+    private $instance = true;
     
     public function __construct()
     {
         echo $this->dump(func_get_args());
     }
-	private function objects($object)
-	{
-		$vals = array();
-		$obj = new ReflectionObject($object);
-		$vals['class'] = $obj->getName();
-		foreach ($obj->getProperties() as $key =>  $prop) {
-			if ($prop->isPrivate()) {
-				$vals[$key]['visibility'] = 'private&nbsp;&nbsp;';
-			}
-			elseif ($prop->isProtected()) {
-				$vals[$key]['visibility'] = 'protected';
-			}
-			elseif ($prop->isPublic()) {
-				$vals[$key]['visibility'] = 'public&nbsp;&nbsp;&nbsp;';	
-			}
-			$getValue = $obj->getDefaultProperties();
-			$vals[$key]['name'] = $prop->getName();
-			$vals[$key]['value'] = $getValue[$prop->getName()];
-		}
-		return $vals;
-	}
+    private function objects($object)
+    {
+        $vals = array();
+        $obj = new ReflectionObject($object);
+        $vals['class'] = $obj->getName();
+        foreach ($obj->getProperties() as $key =>  $prop) {
+			//the &nbsp; is to make sure visibilities are aligned
+            if ($prop->isPrivate()) {
+                $vals[$key]['visibility'] = 'private&nbsp;&nbsp;';
+            }
+            elseif ($prop->isProtected()) {
+                $vals[$key]['visibility'] = 'protected';
+            }
+            elseif ($prop->isPublic()) {
+                $vals[$key]['visibility'] = 'public&nbsp;&nbsp;&nbsp;';    
+            }
+            $getValue = $obj->getDefaultProperties();
+            $vals[$key]['name'] = $prop->getName();
+            $vals[$key]['value'] = $getValue[$prop->getName()];
+        }
+        return $vals;
+    }
     private function dump()
     {
         $args = func_get_args();
-		if ($this->instance) {
-			$args = $args[0];
-			$this->instance = false;
-		}
+        if ($this->instance) {
+            $args = $args[0];
+            $this->instance = false;
+        }
         $dumped = '';
         for ($i = 0; $i < count($args); $i++) 
         {
@@ -88,12 +89,12 @@ class Dump
                 if (!$this->proc_end && $this->marg == 20) {
                     $dumped .= '<code><b style="color:' . self::N_ARRAY . ';">array</b> <i style="color:' .self::DATA_TY . ';">';
                     $dumped .= '(size=' . $length . ')</i> [<br />';
-					if ($length == 0) {
-						$this->marg += 20;
-						$dumped .= '<code style="margin-left:' .$this->marg. 'px;">(empty)</code>';
-						$this->marg -= 20;
-						$dumped .= '<br /><code style="margin-left:' .$this->marg. 'px;">]</code> <br />';
-					}
+                    if ($length == 0) {
+                        $this->marg += 20;
+                        $dumped .= '<code style="margin-left:' .$this->marg. 'px;">(empty)</code>';
+                        $this->marg -= 20;
+                        $dumped .= '<br /><code style="margin-left:' .$this->marg. 'px;">]</code> <br />';
+                    }
                 }
                 foreach ($args[$i] as $key => $values) {
                     if (is_array($values)) {
@@ -126,31 +127,31 @@ class Dump
                     }
                 }
             }
-			elseif ($data_type == 'object') {
-				$object = $this->objects($args[$i]);
-				$dumped .= '<code><b style="color:' . self::N_ARRAY . ';">object</b> <i style="color:' .self::DATA_TY . ';">';
-				$dumped .= '(' . $object['class'] . ')</i><br />';
+            elseif ($data_type == 'object') {
+                $object = $this->objects($args[$i]);
+                $dumped .= '<code><b style="color:' . self::N_ARRAY . ';">object</b> <i style="color:' .self::DATA_TY . ';">';
+                $dumped .= '(' . $object['class'] . ')</i><br />';
                 foreach ($object as $key => $values) {
-					if (is_array($values)) {
-						$dumped .= '<code style="margin-left:' .$this->marg. 'px;">';
-						$dumped .= '<span style="color:'. self::VISIB . '">' . $values['visibility'] . '</span>';
-						$dumped .= '</span> <span style="color:'. self::VAR_N . '">';
-						if (is_array($values['value'])) {
-							$dumped .= '\'' . $values['name'] . '\' </span>';
-							$dumped .= '<span style="color:'. self::PNT . '"> : </span>';
-							$dumped .= $this->dump($values['value']);
-						}
-						else {
-							$dumped .= '\'' . $values['name'] . '\' </span>';
-							$dumped .= '<span style="color:'. self::PNT . '"> : </span>';
-							$dumped .= $this->dump($values['value']);
-						}
-					}
+                    if (is_array($values)) {
+                        $dumped .= '<code style="margin-left:' .$this->marg. 'px;">';
+                        $dumped .= '<span style="color:'. self::VISIB . '">' . $values['visibility'] . '</span>';
+                        $dumped .= '</span> <span style="color:'. self::VAR_N . '">';
+                        if (is_array($values['value'])) {
+                            $dumped .= '\'' . $values['name'] . '\' </span>';
+                            $dumped .= '<span style="color:'. self::PNT . '"> : </span>';
+                            $dumped .= $this->dump($values['value']);
+                        }
+                        else {
+                            $dumped .= '\'' . $values['name'] . '\' </span>';
+                            $dumped .= '<span style="color:'. self::PNT . '"> : </span>';
+                            $dumped .= $this->dump($values['value']);
+                        }
+                    }
                 }
-			}
+            }
         }
          return $dumped;
     }
-	
+    
 
 }
